6886e56690f198a77c7dbe291e8a79a7c2e9fb21
jeeva1429 <116168359+jeeva1429@users.noreply.github.com>
2022-12-14 14:24:43 +0530

    Update game.js

--- game.js
+++ game.js
@@ -22,6 +22,19 @@ const rockHandMachine = document.getElementById('rock-hand-machine')
 const paperHandMachine = document.getElementById('paper-hand-machine')
 const scissorsHandMachine = document.getElementById('scissors-hand-machine')
 
+var score = () =>{
+    if (Humanscore == 5){
+        HideLogoHands(childLogoHands)
+        document.getElementById('human-message').style.display = "inherit"
+        document.getElementById('play-again').style.display = "inherit"
+        
+        }
+    else if(machinescore == 5){
+            HideLogoHands(childLogoHands)
+            document.getElementById('computer-message').style.display = "inherit"
+            document.getElementById('play-again').style.display = "inherit"
+        }
+}
 
 const machineHandimages = document.getElementById('game-hand-div-machine')
 var childImages = machineHandimages.children;
@@ -39,78 +52,42 @@ document.getElementById('play-again').onclick = () => {
 computerResults = [paperHandMachine, scissorsHandMachine,rockHandMachine]
 var Humanscore = 0
 var machinescore = 0
+
 rock.onclick = () => {
     var randomNum = Math.floor(Math.random()*childImages.length)
     rockHand.style.display = "inherit"
     scissorsHand.style.display = "none"
     paperHand.style.display = "none"
-
     hideMachineHandImages(childImages)
     childImages[randomNum].style.display = "block"
-
-    
    if (rockHand.alt == "rock" && computerResults[randomNum].alt == "scissors"){
     Humanscore++
-    
    }
-
    else if (rockHand.alt == "rock" && computerResults[randomNum].alt == "paper"){
     machinescore++
-   
    }
-   if (Humanscore == 5){
-    HideLogoHands(childLogoHands)
-    document.getElementById('human-message').style.display = "inherit"
-    document.getElementById('play-again').style.display = "inherit"
-    
-    }
-    else if(machinescore == 5){
-        HideLogoHands(childLogoHands)
-        document.getElementById('computer-message').style.display = "inherit"
-        document.getElementById('play-again').style.display = "inherit"
-    }
+   score()
   document.getElementById('your-score').innerHTML = Humanscore
   document.getElementById('computer-score').innerHTML = machinescore
-
 }
-
-
 
 scissors.onclick = () => {
     var randomNum = Math.floor(Math.random()*3)
     rockHand.style.display = "none"
     scissorsHand.style.display = "inherit"
     paperHand.style.display = "none"
-
-    
     hideMachineHandImages(childImages)
     childImages[randomNum].style.display = "block"
-
-    
    if (scissorsHand.alt == "scissors" && computerResults[randomNum].alt == "paper"){
     Humanscore++
-    
    }
    else if (scissorsHand.alt == "scissors" && computerResults[randomNum].alt == "rock"){
     machinescore++
-   
    }
-    if (Humanscore == 5){
-        HideLogoHands(childLogoHands)
-        document.getElementById('human-message').style.display = "inherit"
-        document.getElementById('play-again').style.display = "inherit"
-    
-        }
-    else if(machinescore == 5){
-        HideLogoHands(childLogoHands)
-        document.getElementById('computer-message').style.display = "inherit"
-        document.getElementById('play-again').style.display = "inherit"
-    }
+   score()
    document.getElementById('your-score').innerHTML = Humanscore
    document.getElementById('computer-score').innerHTML = machinescore
-
 }
-
 
 
 paper.onclick = () => {
@@ -118,28 +95,15 @@ paper.onclick = () => {
     rockHand.style.display = "none"
     scissorsHand.style.display = "none"
     paperHand.style.display = "inherit"
-    
-   
     hideMachineHandImages(childImages)
     childImages[randomNum].style.display = "block"
-
-
    if (paperHand.alt == "paper" && computerResults[randomNum].alt == "scissors"){
     machinescore++
    }
    else if (paperHand.alt == "paper" && computerResults[randomNum].alt == "rock"){
     Humanscore++
    }
-   if (Humanscore == 5){
-    HideLogoHands(childLogoHands)
-    document.getElementById('human-message').style.display = "inherit"
-    document.getElementById('play-again').style.display = "inherit"
-   }
-   else if(machinescore == 5){
-    HideLogoHands(childLogoHands)
-    document.getElementById('computer-message').style.display = "inherit"
-    document.getElementById('play-again').style.display = "inherit"
-   }
+   score()
    document.getElementById('your-score').innerHTML = Humanscore
    document.getElementById('computer-score').innerHTML = machinescore
 
